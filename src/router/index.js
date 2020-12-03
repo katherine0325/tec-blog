99c2f41bcffbfb8486bcf3d3b11d771cbe0aef73
katherine0325 <510175889@qq.com>
2020-12-03 17:56:44 +0800

    根据路由按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home.vue'
-import Article from '@/pages/article/Article.vue'
 
 Vue.use(Router)
 
@@ -10,12 +8,12 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('@/pages/home/Home.vue')
     },
     {
       path: '/article',
       name: 'Article',
-      component: Article
+      component: () => import('@/pages/article/Article.vue')
     }
   ]
 })
